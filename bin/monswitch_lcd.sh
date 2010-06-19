0ce13266c185d1de187ca7f1adb10091da927034
Abhishek Mishra <ideamonk@gmail.com>
2010-06-19 18:53:01 +0530

    monitor switchers now fix wallpaper too

--- bin/monswitch_lcd.sh
+++ bin/monswitch_lcd.sh
@@ -2,4 +2,5 @@
 xrandr --output VGA1 --auto
 xrandr -s 1920x1080
 xrandr --output LVDS1 --off 
+~/bin/mywall.sh
 
--- bin/monswitch_pebble.sh
+++ bin/monswitch_pebble.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 xrandr --output LVDS1 --auto 
 xrandr --output VGA1 --off 
+~/bin/mywall.sh
 
